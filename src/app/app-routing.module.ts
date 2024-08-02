b89341d419918c8baff83740d3510deac5db9bc0
Jeremy DEQUEANT <jeremy.dequeant2024@campus-eni.fr>
2024-08-02 12:04:54 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AppComponent} from "./app.component";
+import {GenerationsComponent} from "./composants/generations/generations.component";
+import {PokemonsComponent} from "./pokemons/pokemons.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: GenerationsComponent},
+  {path: 'generations', component: GenerationsComponent},
+  {path: 'pokemons', component: PokemonsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
